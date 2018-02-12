2906f53147ac7afa7c1567d4da03a9d77be9418c
Kevin Kennis <kevin@bitgo.com>
2018-02-11 23:27:48 -0800

    highlight current route in sidebar

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,20 +1,23 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
+import classNames from 'classnames';
 import nav from 'constants/nav';
 
-const Sidebar = ({ isLoggedIn }) => {
+// give sidebar the router so we can know which route to highlight
+const Sidebar = withRouter(({ isLoggedIn, location }) => {
   // Don't show unusable sidebar elements
   const navElements = nav.main.filter(({ needsLogin }) => isLoggedIn || !needsLogin);
+  const getCurrentRoute = (section) => section.url === location.pathname && 'selected';
 
   return (
-    <div className="leftNavBar">
+    <div className='leftNavBar'>
       {navElements.map((section, index) =>
-        <div className="leftNav" key={`nav-${index}`}>
-          <Link to={section.url}><div className="navigation">{section.title}</div></Link>
+        <div className={classNames('leftNav', getCurrentRoute(section))} key={`nav-${index}`}>
+          <Link to={section.url}><div className='navigation'>{section.title}</div></Link>
         </div>
       )}
     </div>
   );
-}
+});
 
 export default Sidebar;
