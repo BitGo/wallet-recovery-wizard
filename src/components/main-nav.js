21f0d8b82ca0805b8138bca64e0b9ca6fe317222
Kevin Kennis <kevin@bitgo.com>
2018-02-12 11:25:02 -0800

    disable box to prevent sidebar from flashing

--- src/components/main-nav.js
+++ src/components/main-nav.js
@@ -5,21 +5,31 @@ import classNames from 'classnames';
 import nav from 'constants/nav';
 
 const MainNav = ({ isLoggedIn }) => {
-  const getDisabled = (section) => section.needsLogin && !isLoggedIn && 'disabled';
+  const isDisabled = (section) => section.needsLogin && !isLoggedIn;
+  const getDisabled = (section) => isDisabled(section) && 'disabled';
 
   return (
     <div>
       <h1 className='content-header'>What would you like to do?</h1>
       {nav.main.map((section, index) =>
-        <Link to={section.url} key={`main-nav-${index}`}>
+        <NavBoxContent isDisabled={isDisabled(section)} section={section}>
           <div className={classNames('optionBox', getDisabled(section))}>
             <h2 className='optionBox-title'>{section.title}</h2>
             <p>{section.description}</p>
           </div>
-        </Link>
+        </NavBoxContent>
       )}
     </div>
   );
 };
+
+const NavBoxContent = ({ isDisabled, children, section }) => {
+  if (isDisabled) {
+    return <div>{children}</div>;
+  }
+
+  return <Link to={section.url}>{children}</Link>;
+};
+
 
 export default MainNav;
