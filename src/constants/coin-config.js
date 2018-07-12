15fd24dfc20b8ece4238debe4d9e9706ea0f2303
David Cruz <davidcruz@bitgo.com>
2018-07-12 09:47:46 -0700

    remove testnet coins from coin-config

--- src/constants/coin-config.js
+++ src/constants/coin-config.js
@@ -70,51 +70,6 @@ export default {
         { label: 'Testnet (Kovan)', value: 'test' }
       ]
     },
-    tbtc: {
-      fullName: 'Testnet Bitcoin',
-      supportedRecoveries: ['tbch', 'tltc'],
-      icon: btcIcon,
-      envOptions: [
-        { label: 'Mainnet', value: 'prod' },
-        { label: 'Testnet', value: 'test' },
-      ]
-    },
-    tbch: {
-      fullName: 'Testnet Bitcoin Cash',
-      supportedRecoveries: ['tbtc', 'tltc'],
-      icon: bchIcon,
-      envOptions: [
-        { label: 'Mainnet', value: 'prod' },
-        { label: 'Testnet', value: 'test' },
-      ]
-    },
-    tltc: {
-      fullName: 'Testnet Litecoin',
-      supportedRecoveries: ['tbtc', 'tbch'],
-      icon: ltcIcon,
-      envOptions: [
-        { label: 'Mainnet', value: 'prod' },
-        { label: 'Testnet', value: 'test' },
-      ]
-    },
-    tzec: {
-      fullName: 'Testnet Zcash',
-      supportedRecovieres: [],
-      icon: zecIcon,
-      envOptions: [
-        { label: 'Mainnet', value: 'prod' },
-        { label: 'Testnet', value: 'test' }
-      ]
-    },
-    teth: {
-      fullName: 'Testnet Ethereum',
-      supportedRecoveries: ['tbtc'],
-      icon: ethIcon,
-      envOptions: [
-        { label: 'Mainnet', value: 'prod' },
-        { label: 'Testnet (Kovan)', value: 'test' }
-      ]
-    },
     token: {
       fullName: 'ERC20 Token',
       supportedRecoveries: [],
