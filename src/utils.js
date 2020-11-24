506d51710914bbc9828ad3f40402e76d17d52e13
Otto Allmendinger <otto@bitgo.com>
2020-11-24 17:25:30 +0100

    Simplify recoverWithKeyPath

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,11 @@
 import * as Errors from 'bitgo/dist/src/errors';
 
+const userKeyPaths = ['/0/0', '/0'];
+
+function withCustomKeyPath(params, userKeyPath) {
+  return Object.assign({}, params, { userKeyPath });
+}
+
 /**
  * Call the recover() function with recoveryParams, and try multiple key paths for the user key
  * @param {Object} baseCoin
@@ -15,18 +21,12 @@ import * as Errors from 'bitgo/dist/src/errors';
  */
 export async function recoverWithKeyPath(baseCoin, recoveryParams) {
   const userKeyPaths = ['/0/0', '/0'];
-  let recoveryPrebuild;
 
   for (const path of userKeyPaths) {
-    recoveryParams['userKeyPath'] = path;
-
     try {
-      recoveryPrebuild = await baseCoin.recover(recoveryParams);
       // if we already have a recovery result, then it means the current path we try
       // is the valid user path, and we can return and exit the iteration loop
-      if (recoveryPrebuild) {
-        return recoveryPrebuild;
-      }
+      return await baseCoin.recover(withCustomKeyPath(recoveryParams, path));
     } catch (e) {
       // if this current path we try yields us no inputs to recover, we catch the
       // error and move on to the next iteration and continue trying the remaining paths
@@ -38,8 +38,5 @@ export async function recoverWithKeyPath(baseCoin, recoveryParams) {
   // if we couldn't build a tx here, it must have been the case that there were no inputs available
   // to recover. All the other errors would have been caught and thrown already by the line:
   // if (e.constructor.name !== Errors.ErrorNoInputToRecover.name) { throw new Error(e.message);}
-  if (!recoveryPrebuild) {
-    throw new Errors.ErrorNoInputToRecover();
-  }
-  return recoveryPrebuild;
+  throw new Errors.ErrorNoInputToRecover();
 }
