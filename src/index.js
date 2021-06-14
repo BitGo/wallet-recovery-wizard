3058afd4afabd1fc1487b40c8000d0c7bef53e23
Aaron Reisman <aaronreisman@bitgo.com>
2021-06-14 11:39:48 -0700

    WIP

--- src/index.js
+++ src/index.js
@@ -5,3 +5,11 @@ import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 // registerServiceWorker();
+
+const { ipcRenderer } = require('electron');
+console.log(ipcRenderer.sendSync('synchronous-message', 'ping')); // prints "pong"
+
+ipcRenderer.on('login-success', (event, arg) => {
+  console.log(arg); // prints "pong"
+});
+ipcRenderer.send('login', { username: 'tom', password: 'blah' });
