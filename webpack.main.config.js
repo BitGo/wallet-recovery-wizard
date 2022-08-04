f5b8b5a888ca9ff9a29223d0adae01a39a018069
Aaron Reisman <aaronreisman@bitgo.com>
2022-08-04 15:24:25 -0700

    initial commit

---
+++ webpack.main.config.js
@@ -0,0 +1,17 @@
+/* eslint-env node */
+/* eslint-disable @typescript-eslint/no-var-requires */
+
+module.exports = {
+  /**
+   * This is the main entry point for your application, it's the first file
+   * that runs in the main process.
+   */
+  entry: './src/index.ts',
+  // Put your normal webpack config below here
+  module: {
+    rules: require('./webpack.rules'),
+  },
+  resolve: {
+    extensions: ['.js', '.ts', '.jsx', '.tsx', '.css', '.json'],
+  },
+};
---
+++ webpack.plugins.js
@@ -0,0 +1,6 @@
+/* eslint-env node */
+/* eslint-disable @typescript-eslint/no-var-requires */
+
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+
+module.exports = [new ForkTsCheckerWebpackPlugin()];
