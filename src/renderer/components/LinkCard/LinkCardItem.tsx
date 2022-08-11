202e973f5b754c8a7974344ef772622e9bcaf9cd
benpreiser <benpreiser@bitgo.com>
2022-08-11 14:39:57 -0400

    fix LinkCard animation (in progress)

--- src/renderer/components/LinkCard/LinkCardItem.tsx
+++ src/renderer/components/LinkCard/LinkCardItem.tsx
@@ -1,9 +1,11 @@
+import clsx from 'clsx';
+import * as React from 'react';
+import { Icon, IconGlyph } from '../Icon';
 import * as Polymorphic from '../Polymorphic';
 
 export type LinkCardItemProps = {
   Title: string;
   Description: string;
-  IconRight: React.ReactNode;
 };
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -15,23 +17,40 @@ export const LinkCardItem = Polymorphic.forwardRef<
   typeof LinkCardItemDefaultTag,
   LinkCardItemProps,
   LinkCardItemTag
->(function LinkCardItem(
-  { Title, Description, IconRight, Tag, ...hostProps },
-  ref
-) {
+>(function LinkCardItem({ Title, Description, Tag, ...hostProps }, ref) {
   const Component = Tag ?? LinkCardItemDefaultTag;
+  const icon1 = <IconGlyph Name="chevron-right" />;
+  const icon2 = <IconGlyph Name="arrow-right" />;
+  const icon1Path = icon1.props;
+  const icon2Path = icon2.props;
+  console.log(icon1, icon2);
   return (
     <Component
       // eslint-disable-next-line @typescript-eslint/no-explicit-any
       ref={ref as any}
       {...hostProps}
-      className="tw-flex tw-flex-col tw-border tw-border-solid tw-px-4 tw-py-4 tw-border-gray-700 tw-rounded"
+      className={clsx(
+        'tw-flex tw-flex-col tw-border tw-border-solid tw-px-4 tw-py-4 tw-border-gray-700 tw-rounded',
+        'hover:tw-bg-gray-100'
+      )}
     >
       <div className="tw-gap-1 tw-flex tw-items-center">
         <span className="tw-text-slate-900 tw-font-medium tw-text-label-1">
           {Title}
         </span>
-        {IconRight && <span>{IconRight}</span>}
+        <span>
+          <svg viewBox="0 0 16 16">
+            <path fill="#000">
+              <animate
+                attributeName="d"
+                dur="1000ms"
+                repeatCount="indefinite"
+                //values={`${icon1Path.props.d};${icon2Path.props.d}`}
+              />
+            </path>
+          </svg>
+          <Icon Name="arrow-right" Size="small" />
+        </span>
       </div>
       <span className="tw-text-gray-700 tw-text-label-2 tw-mt-1">
         {Description}
