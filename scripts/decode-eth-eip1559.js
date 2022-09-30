9a85a6004af4e91086f655eda1b194dd235f846b
benpreiser <benpreiser@bitgo.com>
2022-09-30 12:08:18 -0400

    chore: remove prompt-sync module

--- scripts/decode-eth-eip1559.js
+++ scripts/decode-eth-eip1559.js
@@ -1,13 +1,16 @@
 const { Transaction: TransactionFactory, FeeMarketEIP1559Transaction } = require('@ethereumjs/tx');
 const ethUtil = require('ethereumjs-util');
-const prompt = require('prompt-sync')();
+const readline = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
 main();
 
-async function main() {
-
-	const data = '0x' + prompt('Enter the transaction hex from your recovery JSON: ');
-	const output = FeeMarketEIP1559Transaction.fromSerializedTx(ethUtil.toBuffer(data));
-
+function main() {
+  readline.question('Enter the transaction hex from your recovery JSON: ', (data) => {
+    const output = FeeMarketEIP1559Transaction.fromSerializedTx(ethUtil.toBuffer('0x' + data));
     console.log(output.toJSON());
+    readline.close();
+  });
 }
