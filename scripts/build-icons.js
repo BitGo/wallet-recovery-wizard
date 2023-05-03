5251067f01eb1278dc883add0213344c8df774a3
Dinshaw Kothari <dinshawkothari@bitgo.com>
2023-05-03 13:06:31 +0530

    feat: onboarding atom coin
    TICKET: BG-76544

--- scripts/build-icons.js
+++ scripts/build-icons.js
@@ -24,6 +24,7 @@ const coins = [
   'xec',
   'doge',
   'ada',
+  'atom',
 ];
 
 const paths = fs
---
+++ src/containers/NonBitGoRecoveryCoin/CosmosForm.tsx
@@ -0,0 +1,150 @@
+import { Form, FormikHelpers, FormikProvider, useFormik } from 'formik';
+import { Link } from 'react-router-dom';
+import * as Yup from 'yup';
+import {
+  Button,
+  FormikSelectfield,
+  FormikTextarea,
+  FormikTextfield,
+} from '~/components';
+
+const validationSchema = Yup.object({
+  backupKey: Yup.string().required(),
+  bitgoKey: Yup.string().required(),
+  krsProvider: Yup.string()
+    .oneOf(['keyternal', 'bitgoKRSv2', 'dai'])
+    .label('Key Recovery Service'),
+  recoveryDestination: Yup.string().required(),
+  scan: Yup.number().required(),
+  startingScanIndex: Yup.number().required(),
+  userKey: Yup.string().required(),
+  walletPassphrase: Yup.string().required(),
+}).required();
+
+export type CosmosFormProps = {
+  onSubmit: (
+    values: CosmosFormValues,
+    formikHelpers: FormikHelpers<CosmosFormValues>
+  ) => void | Promise<void>;
+};
+
+type CosmosFormValues = Yup.Asserts<typeof validationSchema>;
+
+export function CosmosForm({ onSubmit }: CosmosFormProps) {
+  const formik = useFormik<CosmosFormValues>({
+    onSubmit,
+    initialValues: {
+      backupKey: '',
+      bitgoKey: '',
+      krsProvider: '',
+      recoveryDestination: '',
+      scan: 20,
+      startingScanIndex: 0,
+      userKey: '',
+      walletPassphrase: '',
+    },
+    validationSchema,
+  });
+
+  const backupKeyHelperText =
+    formik.values.krsProvider === ''
+      ? 'Your encrypted backup key, as found on your BitGo recovery keycard.'
+      : 'The backup public key for the wallet, as found on your BitGo recovery keycard.';
+
+  return (
+    <FormikProvider value={formik}>
+      <Form>
+        <h4 className="tw-text-body tw-font-semibold tw-border-b-0.5 tw-border-solid tw-border-gray-700 tw-mb-4">
+          Self-managed hot wallet details
+        </h4>
+        <div className="tw-mb-4">
+          <FormikSelectfield
+            HelperText="The Key Recovery Service that you chose to manage your backup key. If you have the encrypted backup key, you may leave this blank."
+            Label="Key Recovery Service"
+            name="krsProvider"
+            Width="fill"
+          >
+            <option value="">None</option>
+            <option value="keyternal">Keyternal</option>
+            <option value="bitgoKRSv2">BitGo KRS</option>
+            <option value="dai">Coincover</option>
+          </FormikSelectfield>
+        </div>
+        <div className="tw-mb-4">
+          <FormikTextarea
+            HelperText="Your encrypted user key, as found on your recovery KeyCard."
+            Label="Box A Value"
+            name="userKey"
+            rows={4}
+            Width="fill"
+          />
+        </div>
+        <div className="tw-mb-4">
+          <FormikTextarea
+            HelperText={backupKeyHelperText}
+            Label="Box B Value"
+            name="backupKey"
+            rows={4}
+            Width="fill"
+          />
+        </div>
+        <div className="tw-mb-4">
+          <FormikTextarea
+            HelperText="The BitGo public key for the wallet, as found on your recovery KeyCard."
+            Label="Box C Value"
+            name="bitgoKey"
+            rows={2}
+            Width="fill"
+          />
+        </div>
+        <div className="tw-mb-4">
+          <FormikTextfield
+            HelperText="The passphrase of the wallet."
+            Label="Wallet Passphrase"
+            name="walletPassphrase"
+            type="password"
+            Width="fill"
+          />
+        </div>
+        <div className="tw-mb-4">
+          <FormikTextfield
+            HelperText="The address your recovery transaction will send to."
+            Label="Destination Address"
+            name="recoveryDestination"
+            Width="fill"
+          />
+        </div>
+        <div className="tw-mb-4">
+          <FormikTextfield
+            HelperText="The amount of addresses without transactions to scan before stopping the tool."
+            Label="Address Scanning Factor"
+            name="scan"
+            Width="fill"
+          />
+        </div>
+        <div className="tw-mb-4">
+          <FormikTextfield
+            HelperText="This is the index to start scanning from, typically used to scan more indexes. You can call this recover function again starting from scanIndex + 1."
+            Label="Starting Scan Index"
+            name="startingScanIndex"
+            Width="fill"
+          />
+        </div>
+        <div className="tw-flex tw-flex-col-reverse sm:tw-justify-between sm:tw-flex-row tw-gap-1 tw-mt-4">
+          <Button Tag={Link} to="/" Variant="secondary" Width="hug">
+            Cancel
+          </Button>
+          <Button
+            Variant="primary"
+            Width="hug"
+            type="submit"
+            Disabled={formik.isSubmitting}
+            disabled={formik.isSubmitting}
+          >
+            {formik.isSubmitting ? 'Recovering...' : 'Recover Funds'}
+          </Button>
+        </div>
+      </Form>
+    </FormikProvider>
+  );
+}
