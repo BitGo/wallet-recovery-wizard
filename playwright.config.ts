c305425d481fdcbd32060d345245ffaac3531bee
Luis Covarrubias <luiscovarrubias@bitgo.com>
2023-09-01 09:25:57 -0700

    chore: increase test timeout

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,13 +12,13 @@ import type { PlaywrightTestConfig } from '@playwright/test';
 const config: PlaywrightTestConfig = {
   testDir: './e2e',
   /* Maximum time one test can run for. */
-  timeout: 30 * 1000,
+  timeout: 60 * 1000,
   expect: {
     /**
      * Maximum time expect() should wait for the condition to be met.
      * For example in `await expect(locator).toHaveText();`
      */
-    timeout: 5000,
+    timeout: 10000,
   },
   /* Run tests in files in parallel */
   fullyParallel: true,
