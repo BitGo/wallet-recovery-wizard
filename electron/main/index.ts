422649c14274b954ab62ce6e0014812755e66eb6
Bhavi Dhingra <bhavidhingra@bitgo.com>
2024-08-02 01:18:23 +0530

    feat(sui): non-bitgo recovery for hot wallets
    WIN-3274

--- electron/main/index.ts
+++ electron/main/index.ts
@@ -55,6 +55,7 @@ import { Hbar, Thbar } from '@bitgo/sdk-coin-hbar';
 import { Algo, Talgo } from '@bitgo/sdk-coin-algo';
 import { Sui, Tsui } from '@bitgo-beta/sdk-coin-sui';
 import { EthLikeCoin, TethLikeCoin } from '@bitgo/sdk-coin-ethlike';
+import { Sui, Tsui } from '@bitgo/sdk-coin-sui';
 
 const bip32 = BIP32Factory(ecc);
 
@@ -144,6 +145,8 @@ sdk.register('sui', Sui.createInstance);
 sdk.register('tsui', Tsui.createInstance);
 sdk.register('baseeth', EthLikeCoin.createInstance);
 sdk.register('tbaseeth', TethLikeCoin.createInstance);
+sdk.register('sui', Sui.createInstance);
+sdk.register('tsui', Tsui.createInstance);
 
 Erc20Token.createTokenConstructors().forEach(({ name, coinConstructor }) => {
   sdk.register(name, coinConstructor);
